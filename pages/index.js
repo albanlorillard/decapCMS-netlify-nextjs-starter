881abd82aeddec23313750e72d0422bfce9b903e
Alban LORILLARD <ext.alban.lorillard@sncf.fr>
2024-02-10 22:22:48 +0100

    decapCMS

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
+
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
+import { attributes, react as HomeContent } from './content/pages/home.md'
 export default function Home() {
+
+  let { title, cats } = attributes
   return (
-    <div className="container">
-      <Head>
-        <title>Alban</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Hello <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+    <>
+    <Head>
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+    </Head>
+    <article>
+      <h1>{title}</h1>
+      <HomeContent />
+      <ul>
+        {cats.map((cat, k) => (
+          <li key={k}>
+            <h2>{cat.name}</h2>
+            <p>{cat.description}</p>
+          </li>
+        ))}
+      </ul>
+    </article>
+  </>
   )
 }
